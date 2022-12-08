fdd669037b910ce66f564030e9e6a5de0554684a
islamGharib <islamahmed1992@yahoo.com>
2022-12-08 16:12:07 +0200

    adding initial packages and files

---
+++ lib/layout/responsive_screen_layout.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/shared/styles/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScreenLayout;
+  final Widget webScreenLayout;
+
+  const ResponsiveLayout({Key? key, required this.mobileScreenLayout, required this.webScreenLayout}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+        builder: (context, constraints){
+          if(constraints.maxWidth > webScreenSize){
+            // web screen
+            return webScreenLayout;
+          }
+          // mobile screen
+          return mobileScreenLayout;
+
+        }
+    );
+  }
+}
---
+++ lib/shared/styles/themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_flutter/shared/styles/colors.dart';
+
+ThemeData mobileTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: mobileBackgroundColor,
+);
