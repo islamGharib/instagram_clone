1f511793e1a0e205b854bfdb2c4b3daeb49eade4
islamGharib <islamahmed1992@yahoo.com>
2022-12-10 21:05:45 +0200

    finishing login screen design

--- lib/modules/login/login_screen.dart
+++ lib/modules/login/login_screen.dart
@@ -1,32 +1,113 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_flutter/shared/component/components.dart';
 import 'package:instagram_flutter/shared/styles/colors.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailControler = TextEditingController();
+
+  final TextEditingController _passControler = TextEditingController();
+
+  @override
+  void dispose(){
+    super.dispose();
+    _emailControler.dispose();
+    _passControler.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 32),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Flexible(child: Container(), flex: 2,),
-            //  svg image
-            SvgPicture.asset(
-              'assets/images/ic_instagram.svg',
-              color: primaryColor,
-              height: 64,
-            ),
-            const SizedBox(height: 64,),
-            
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 32),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(child: Container(), flex: 2,),
+              //  svg image
+              SvgPicture.asset(
+                'assets/images/ic_instagram.svg',
+                color: primaryColor,
+                height: 64,
+              ),
+              const SizedBox(height: 64,),
+              // textformfield for email
+              DefaultTextFormField(
+                  controller: _emailControler,
+                  type: TextInputType.emailAddress,
+                  validate: (value){},
+                  label: 'Enter your email',
+                  prefix: Icons.email
+              ),
+              const SizedBox(height: 24,),
+              // textformfield for pass
+              DefaultTextFormField(
+                  controller: _passControler,
+                  type: TextInputType.text,
+                  validate: (value){},
+                  label: 'Enter your pass',
+                  prefix: Icons.lock,
+                  isPassword: true,
+                  suffix: Icons.visibility_outlined,
+              ),
+              const SizedBox(height: 24,),
+              //  login button
+              InkWell(
+                child: Container(
+                  child: const Text('Login'),
+                  width: double.infinity,
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  decoration: const ShapeDecoration(shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                  ),
+                    color: blueColor,
+                  ),
+
+                ),
+              ),
+              const SizedBox(height: 24,),
+              Flexible(child: Container(), flex: 2,),
+              //  transitioning to signing up
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    child: const Text(
+                      'Don\'t have an account?',
+                    ),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Container(
+                      child: const Text(
+                        'Sign up.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                    ),
+                  ),
+                ],
+              ),
+
+              
 
 
-          ],
+
+
+            ],
+          ),
         ),
       ),
     );
--- lib/shared/component/components.dart
+++ lib/shared/component/components.dart
@@ -1,59 +1,21 @@
 import 'package:flutter/material.dart';
 
-// Text Form Field
-// Widget defaultTextFormField(
-//     {
-//       required final TextEditingController? controller,
-//       required TextInputType type,
-//       required  String? Function(String?)? validate,
-//       required String label,
-//       required IconData prefix,
-//       required OutlineInputBorder inputBorder,
-//       bool isPassword = false,
-//       IconData? suffix,
-//       Function()? suffixPressed,
-//       Function()? onTap,
-//       Function(String)? onChange,
-//       Function(String)? onSubmit,
-//
-//     }) => TextFormField(
-//     controller: controller,
-//     keyboardType: type,
-//     validator: validate,
-//     obscureText: isPassword,
-//     onTap: onTap,
-//     onChanged: onChange,
-//     onFieldSubmitted: onSubmit,
-//     decoration: InputDecoration(
-//       labelText: label,
-//       prefixIcon: Icon(
-//           prefix
-//       ),
-//       suffixIcon: suffix != null?IconButton(
-//           onPressed: suffixPressed,
-//           icon: Icon(suffix,)):null,
-//       border:  inputBorder,
-//       focusedBorder: inputBorder,
-//       enabledBorder: inputBorder
-//
-//     )
-// );
-
 class DefaultTextFormField extends StatelessWidget {
   final TextEditingController? controller;
   final TextInputType type;
   final String? Function(String?)? validate;
   final String label;
   final IconData prefix;
-  bool isPassword = false;
-  IconData? suffix;
-  Function()? suffixPressed;
-  Function()? onTap;
-  Function(String)? onChange;
-  Function(String)? onSubmit;
+  final bool? isPassword ;
+  final IconData? suffix;
+  final Function()? suffixPressed;
+  final Function()? onTap;
+  final Function(String)? onChange;
+  final Function(String)? onSubmit;
 
-  DefaultTextFormField({Key? key, required this.controller, required this.type,
-  required this.validate, required this.label, required this.prefix,}) : super(key: key);
+  const DefaultTextFormField({Key? key, this.isPassword = false, this.suffix, this.suffixPressed,
+    this.onTap, this.onChange, this.onSubmit, required this.controller, required this.type, this.validate, required this.label, required this.prefix
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -64,7 +26,7 @@ class DefaultTextFormField extends StatelessWidget {
       controller: controller,
       keyboardType: type,
       validator: validate,
-      obscureText: isPassword,
+      obscureText: isPassword!,
       onTap: onTap,
       onFieldSubmitted: onSubmit,
       onChanged: onChange,
